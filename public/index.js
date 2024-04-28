b6133030705f1b388d3e0eb18d1489d70a9e9907
kaspermoelans <104373919+kaspermoelans@users.noreply.github.com>
2024-04-28 19:33:56 +0200

    Update index.js

--- public/index.js
+++ public/index.js
@@ -206,20 +206,20 @@ window.addEventListener("touchstart", (e) => {
       const clientX = touch.clientX
       const clientY = touch.clientY
 
-      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: 10, y: canvasEl.height * 0.80, w: 168, h: 144})) {
+      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: 10, y: canvasEl.height - 200, w: 168, h: 144})) {
         inputs['left'] = true
         socket.emit('inputs', inputs)
       }
-      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: 168, y: canvasEl.height * 0.80, w: 168, h: 144})) {
+      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: 168, y: canvasEl.height - 200, w: 168, h: 144})) {
         inputs['right'] = true
         socket.emit('inputs', inputs)
       }
 
-      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x:  canvasEl.width - 168 - 192, y: canvasEl.height * 0.80, w: 168, h: 144})) {
+      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x:  canvasEl.width - 168 - 192, y: canvasEl.height - 200, w: 168, h: 144})) {
         inputs['up'] = true
         socket.emit('inputs', inputs)
       }
-      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: canvasEl.width - 10 - 192, y: canvasEl.height * 0.80, w: 168, h: 144})) {
+      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: canvasEl.width - 10 - 192, y: canvasEl.height - 200, w: 168, h: 144})) {
         inputs['dash'] = true
         socket.emit('inputs', inputs)
       }
@@ -243,20 +243,20 @@ window.addEventListener("touchend", (e) => {
       const clientX = touch.clientX
       const clientY = touch.clientY
 
-      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: 10, y: canvasEl.height * 0.80, w: 168, h: 144})) {
+      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: 10, y: canvasEl.height - 200, w: 168, h: 144})) {
         inputs['left'] = false
         socket.emit('inputs', inputs)
       }
-      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: 168, y: canvasEl.height * 0.80, w: 168, h: 144})) {
+      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: 168, y: canvasEl.height - 200, w: 168, h: 144})) {
         inputs['right'] = false
         socket.emit('inputs', inputs)
       }
 
-      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x:  canvasEl.width - 168 - 192, y: canvasEl.height * 0.80, w: 168, h: 144})) {
+      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x:  canvasEl.width - 168 - 192, y: canvasEl.height - 200, w: 168, h: 144})) {
         inputs['up'] = false
         socket.emit('inputs', inputs)
       }
-      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: canvasEl.width - 10 - 192, y: canvasEl.height * 0.80, w: 168, h: 144})) {
+      if (isColliding({x: clientX, y: clientY, w: 1, h: 1}, {x: canvasEl.width - 10 - 192, y: canvasEl.height - 200, w: 168, h: 144})) {
         inputs['dash'] = false
         socket.emit('inputs', inputs)
       }
@@ -387,10 +387,10 @@ function loop() {
 
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   if (isMobile) {
-    canvas.drawImage(leftImage, 20, canvasEl.height * 0.80)
-    canvas.drawImage(rightImage, 20 + 20 + 192, canvasEl.height * 0.80)
-    canvas.drawImage(upImage, canvasEl.width - 20 - 20 - 192 - 192, canvasEl.height * 0.80)
-    canvas.drawImage(dashImage, canvasEl.width - 20 - 192, canvasEl.height * 0.80)
+    canvas.drawImage(leftImage, 20, canvasEl.height - 200)
+    canvas.drawImage(rightImage, 20 + 20 + 192, canvasEl.height - 200)
+    canvas.drawImage(upImage, canvasEl.width - 20 - 20 - 192 - 192, canvasEl.height - 200)
+    canvas.drawImage(dashImage, canvasEl.width - 20 - 192, canvasEl.height - 200)
     canvas.drawImage(skinImage, canvasEl.width - 20 - 64, canvasEl.height * 0.05)
     canvas.drawImage(taggedImage, 20, canvasEl.height * 0.05)
   }
