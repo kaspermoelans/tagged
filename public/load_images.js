5d1cceac64379fdd7e0311bed33b9ae17f8fd388
kaspermoelans <104373919+kaspermoelans@users.noreply.github.com>
2025-04-21 13:46:51 +0200

    Animation update

--- public/load_images.js
+++ public/load_images.js
@@ -16,63 +16,17 @@ const taggedImage = new Image();
 taggedImage.src = "img/tagged.png";
 
 // Skins
-const right_taggedImage = new Image();
-right_taggedImage.src = "img/right_tagged.png";
-const left_taggedImage = new Image();
-left_taggedImage.src = "img/left_tagged.png";
-
-const right_red_santaImage = new Image();
-right_red_santaImage.src = "img/right_red_santa.png";
-const left_red_santaImage = new Image();
-left_red_santaImage.src = "img/left_red_santa.png";
-
-const right_pink_santaImage = new Image();
-right_pink_santaImage.src = "img/right_pink_santa.png";
-const left_pink_santaImage = new Image();
-left_pink_santaImage.src = "img/left_pink_santa.png";
-
-const right_bananaImage = new Image();
-right_bananaImage.src = "img/right_banana.png";
-const left_bananaImage = new Image();
-left_bananaImage.src = "img/left_banana.png";
-
-const right_tomatoImage = new Image();
-right_tomatoImage.src = "img/right_tomato.png";
-const left_tomatoImage = new Image();
-left_tomatoImage.src = "img/left_tomato.png";
-
-const right_vikingImage = new Image();
-right_vikingImage.src = "img/right_viking.png";
-const left_vikingImage = new Image();
-left_vikingImage.src = "img/left_viking.png";
-
-const right_ninjaImage = new Image();
-right_ninjaImage.src = "img/right_ninja.png";
-const left_ninjaImage = new Image();
-left_ninjaImage.src = "img/left_ninja.png";
-
-const right_pink_dudeImage = new Image();
-right_pink_dudeImage.src = "img/right_pink_dude.png";
-const left_pink_dudeImage = new Image();
-left_pink_dudeImage.src = "img/left_pink_dude.png";
-
-const right_white_dudeImage = new Image();
-right_white_dudeImage.src = "img/right_white_dude.png";
-const left_white_dudeImage = new Image();
-left_white_dudeImage.src = "img/left_white_dude.png";
-
-const right_blue_dudeImage = new Image();
-right_blue_dudeImage.src = "img/right_blue_dude.png";
-const left_blue_dudeImage = new Image();
-left_blue_dudeImage.src = "img/left_blue_dude.png";
-
-const right_appeltaartImage = new Image();
-right_appeltaartImage.src = "img/right_appeltaart.png";
-const left_appeltaartImage = new Image();
-left_appeltaartImage.src = "img/left_appeltaart.png";
-
-const blackImage = new Image();
-blackImage.src = "img/black.png";
+let skinsNames = ["stonie", "energon", "barbarian", "ninja", "earthy", "bo"]
+let skins = []
+let skinsTagged = []
+let i = 0
+skinsNames.forEach(skin => {
+    skins.push(new Image())
+    skins[i].src = "img/" + skin + ".png";
+    skinsTagged.push(new Image())
+    skinsTagged[i].src = "img/" + skin + "Tagged.png";
+    i ++
+})
 
 // Boosts
 const invisibilityImage = new Image();
